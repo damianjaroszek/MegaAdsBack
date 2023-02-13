1929d3ee430566b0fe95f6f5112bd59e16f545dc
damianjaroszek <deejay.8lm@gmail.com>
2023-02-13 19:21:21 +0100

    Delete config.ts

--- config/config.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-    dbHost: 'localhost',
-    dbUser: 'root',
-    dbPassword: '',
-    dbDatabase: 'megak_ads',
-};
-
-// do przechowywania rzeczy, które są zmienne np. jak połączenie z bazą
