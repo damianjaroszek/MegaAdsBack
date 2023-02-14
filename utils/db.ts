2d51565fbe09252b9181ea10f3f91b1a34760a88
DJ <damianjaroszek90@gmail.com>
2023-02-14 19:52:27 +0100

    Fixed config.ts - db.Password

--- utils/db.ts
+++ utils/db.ts
@@ -5,6 +5,7 @@ export const pool = createPool({
     host: config.dbHost,
     user: config.dbUser,
     //password: '', // nie udostępniamy swoich haseł na github, warto ten plik dodać do .gitignore --> utils/db.ts
+    //password: config.dbPassword,
     database: config.dbDatabase,
     namedPlaceholders: true,
     decimalNumbers: true,
