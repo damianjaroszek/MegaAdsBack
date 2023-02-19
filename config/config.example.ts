eee5f8de5074d5641b29c5a76aacbc071b61edea
DJ <damianjaroszek90@gmail.com>
2023-02-19 14:10:56 +0100

    Added CORS origin to config

--- config/config.example.ts
+++ config/config.example.ts
@@ -3,6 +3,7 @@
 //     dbUser: 'user',
 //     dbPassword: 'password',
 //     dbDatabase: 'database',
+//     corsOrigin: 'http://localhost:3000',
 // };
 
 // do przechowywania rzeczy, które są zmienne np. jak połączenie z baządsd
